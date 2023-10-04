15146de696d8e2e36fc9a7798e28f316e010eed7
vanshgautam03 <vanshgautam620@gmail.com>
2023-10-04 14:06:35 -0400

    Assignment 1 start

---
+++ js/storymaker.js
@@ -0,0 +1,55 @@
+// Assignment 1 | COMP1073 Client-Side JavaScript
+
+/* Variables
+-------------------------------------------------- */
+// Constants for main button query selectors
+
+
+// Constants for p tag to display query selectors
+
+
+// Constants for final buttons and p tags
+
+
+// Variables for pre-defined arrays
+
+
+// Variables for count to grab array elements
+
+
+/* Functions
+-------------------------------------------------- */
+function noun1_on_click() {
+    // variable to get array element and displaying it
+
+    // if-else to change count setting
+}
+
+function verb_on_click() {
+    
+}
+
+function adjective_on_click() {
+    
+}
+
+function noun2_on_click() {
+    
+}
+
+function setting_on_click() {
+    
+}
+
+// concatenate the user story and display
+function playback_on_click() {
+    
+}
+
+// grabbing random element from arrays, concatenate and display
+function random_on_click() {
+    
+}
+
+/* Event Listeners
+-------------------------------------------------- */
